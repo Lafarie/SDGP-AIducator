e06a37700f8a016e294d0badd80f073a88846b83
Sathindu Perera <sathindu.geethika@gmail.com>
2024-02-05 13:29:19 +0530

    adding waiting animation

--- client/src/AIAssistant.js
+++ client/src/AIAssistant.js
@@ -1,11 +1,27 @@
-import { useEffect, useRef } from 'react';
+import {useRef, useState} from 'react';
 import './AIAssistant.css';
 import sendIcon from './Images/sendicon.svg'
 import good from './Images/thumbsup.svg'
 import bad from './Images/thumbsdown.svg'
 
+var responseInt = 0;
+
+function untilRespond(element){
+    let msg = "Wait a moment";
+    responseInt = setInterval(()=>{
+        if(msg.substring(msg.length-3) !== "..."){
+            element.innerHTML = (msg += ".");
+        } else {
+            msg = msg.substring(0, msg.length - 3)
+            element.innerHTML = (msg += ".");
+        }
+        }, 1000);
+}
+
 function Assistant(){
     let prompt = useRef();
+
+    const [textbox, settextBox] = useState(false);
 
     function handlePrompt(){
         let promptObj = {
@@ -15,24 +31,36 @@ function Assistant(){
             method: "post",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(promptObj)
-        }).then((response) => response.json()).then((data) => {document.getElementById("response").innerHTML = data.generated_result});
+        }).then((response) => response.json()).then((data) => {
+            document.getElementById("response").style.fontSize = "16px";
+            document.getElementById("response").innerHTML = data.generated_result
+            settextBox(false);
+            clearInterval(responseInt);
+        });
     }
+    
     return(
       <div id={"AIAssistant"}>
         <div id={"Assistant"} className={'frames'}>
             <div id={"assistentFrame"}>
-                <input type='text' onKeyDownCapture={(e) => {
+                <input autoFocus type='text' onKeyDownCapture={(e) => {
                     if(e.key === 'Enter'){
-                        document.getElementById("response").innerHTML = "Wait a moment...";
-                        handlePrompt(); // getting value of prompt when enter is pressed
+                        document.getElementById("response").style.fontSize = "20px";
+                        document.getElementById("response").innerHTML = "Wait a moment";
+                        untilRespond(document.getElementById("response"));
+                        settextBox(true);
+                        handlePrompt();
                     }
-                }} id={'prompt'} placeholder='Enter question here...' ref={prompt} />
+                }} id={'prompt'} placeholder='Enter question here...' ref={prompt} disabled={textbox}/>
                 <div id={'generate'} className='button'><img src={sendIcon} alt='prompt send icon' onClick={() => {
-                    document.getElementById("response").innerHTML = "Wait a moment...";
+                    document.getElementById("response").style.fontSize = "20px";
+                    document.getElementById("response").innerHTML = "Wait a moment";
+                    untilRespond(document.getElementById("response"));
+                    settextBox(true);
                     handlePrompt(); // getting value of prompt when send button is pressed. 
                 }}/></div>
             </div>
-            <div id={'response'}><p style={{textAlign: "center", lineHeight: "200%"}}>Hello there!<br/>I am AIducator an AI assistant here to assist you in your educational journey.
+            <div id={'response'}><p style={{textAlign: "center", lineHeight: "200%", fontSize: "20px"}}>Hello there!<br/>I am AIducator an AI assistant here to assist you in your educational journey.
             <br/><br/>I can answer any educational question you have.<br/>All you gotta do is ask me :D.</p></div> 
             <div id={'rating'}>
                 <div id={'good'} className='button'><img src={good} alt='prompt send icon'/></div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './App';
+import Video from './video';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
@@ -10,7 +11,7 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home/>}/>
-      <Route path="/video" element={<Home/>}/> {/*Add the required element to be rendered in the element attribute (according to the path specified)*/}
+      <Route path="/video" element={<Video/>}/> {/*Add the required element to be rendered in the element attribute (according to the path specified)*/}
       <Route path="/quiz" element={<Home/>}/>
       <Route path="/forum" element={<Home/>}/>
       <Route path="/todo" element={<Home/>}/>
