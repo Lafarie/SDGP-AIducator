d68cbf6815c00b8615bbe3d130299761959c2193
Farhad Lafarie <farhadlafarie@gmail.com>
2024-02-13 11:36:13 +0530

    3d library

---
+++ client/src/3DItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Navbar } from './App';
+import './index.css';
+import './AIAssistant.css';
+import './App.css';
+import './3DLibrary.css';
+import 
+
+function Iteam(){
+    return (
+        <div>
+            <h1>3D models</h1>
+            <div id={"models"} className={'frames'}></div>
+        </div>
+    )
+}
