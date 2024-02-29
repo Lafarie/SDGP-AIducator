77cb098bb32902e3b81368eca537b68061705ba4
Sathindu Perera <sathindu.geethika@gmail.com>
2024-02-29 14:09:09 +0530

    Intilized firebase, added form funtionalies to the user detail form

--- client/src/SignUp.jsx
+++ client/src/SignUp.jsx
@@ -1,19 +1,33 @@
 import "./SIgnUp.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import hide from "./Images/eye-password-hide.svg";
 import show from "./Images/eye-password-show.svg";
 import googleIcon from "./Images/GoogleIcon.svg";
 import appleIcon from "./Images/AppleIcon.svg";
 import logo from "./Images/BigLogo.png";
-
-// add icons to sign in with ... buttons
-// add required to all feilds
-// style sign in with ... buttons 
-// make left panel 
-// style check box 
-// add button click styles. 
+import app from "./firebase";
 
 function SignUp(){
+
+    let firstName = useRef();
+    let lastName = useRef();
+    let email = useRef();
+    let password = useRef();
+
+    function getUserDetails(e){
+        e.preventDefault();
+
+        let userObj = {
+            fname : firstName.current.value,
+            lname : lastName.current.value,
+            email: email.current.value,
+            password: password.current.value
+        }
+
+
+        console.log(userObj)
+    }
+
     useEffect(() => {
         document.body.id = "SignUpBody";
         // Clean up function to remove styles on unmount (optional)
@@ -27,18 +41,18 @@ function SignUp(){
     return(
         <div id="signUp">
             <div id="formDiv" className="panel">
-                <form>
+                <form onSubmit={getUserDetails}>
                     <h1 style={{"color": "#003366"}} className="panelItem">Create an Account</h1>
                     <div id="nameDiv" className="textBoxDiv panelItem">
-                        <input type="text" placeholder="First Name" autoFocus required/>
-                        <input type="text" placeholder="Last Name" required/>
+                        <input type="text" placeholder="First Name" ref={firstName} autoFocus required/>
+                        <input type="text" placeholder="Last Name" ref={lastName} required/>
                         
                     </div>
                     <div id="mailDiv" className="textBoxDiv panelItem">
-                        <input type="text" placeholder="Email Address" required/>
+                        <input type="text" placeholder="Email Address" ref={email} required/>
                     </div>
                     <div id="passDiv" className="textBoxDiv panelItem">
-                        <input id="passwordtxt" type={showPass?"text":"password"} required placeholder="Your Password"/>
+                        <input id="passwordtxt" type={showPass?"text":"password"} ref={password} required placeholder="Your Password"/>
                         <img src={showPass?show:hide} onClick={() => {
                             setShowPass(!showPass);
                         }}/>
@@ -48,7 +62,7 @@ function SignUp(){
                     </button>
                     <div>
                         <input type="checkbox" id="terms" required/>
-                        <label htmlFor="terms">By submitting the form, you agree to our Terms and Conditions</label>
+                        <label htmlFor="terms" style={{cursor : "pointer", marginLeft: "10px"}}>By submitting the form, you agree to our Terms and Conditions</label>
                     </div>
                     <div id="divider">
                         <div>
