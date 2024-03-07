4ffbee8079ec71cc68272c12b1b1a330980d6b7c
Nilusha Muthumala <nilusha200209@gmail.com>
2024-03-07 10:40:49 +0530

    node modules delete

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,30 +9,30 @@ import QuizHome from './QuizHome';
 import Library from './3DLibrary';
 import SignUp from './SignUp'
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LessonList from './LessonList';
 
 //react-scripts
-  // "scripts": {
-  //   "start": "react-scripts start",
-  //   "build": "react-scripts build",
-  //   "test": "react-scripts test",
-  //   "eject": "react-scripts eject"
-  // }
+// "scripts": {
+//   "start": "react-scripts start",
+//   "build": "react-scripts build",
+//   "test": "react-scripts test",
+//   "eject": "react-scripts eject"
+// }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<AIAssistant/>}/>
-      <Route path="/video" element={<Video/>}/> {/*Add the required element to be rendered in the element attribute (according to the path specified)*/}
-      <Route path="/quiz" element={<QuizHome/>}/>
-      <Route path="/forum" element={<Forum/>}/>
-      <Route path="/todo" element={<Home/>}/>
-      <Route path="/profile" element={<SignUp/>}/>
-      <Route path="/library" element={<Library/>}/>
-      <Route path="/LessonList" element={<LessonList subject={'Mathematics'}/>}/>
+      <Route path="/" element={<AIAssistant />} />
+      <Route path="/video" element={<Video />} /> {/*Add the required element to be rendered in the element attribute (according to the path specified)*/}
+      <Route path="/quiz" element={<QuizHome />} />
+      <Route path="/forum" element={<Forum />} />
+      <Route path="/todo" element={<Home />} />
+      <Route path="/profile" element={<SignUp />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/LessonList" element={<LessonList subject={'Mathematics'} />} />
     </Routes>
   </BrowserRouter>
 );
